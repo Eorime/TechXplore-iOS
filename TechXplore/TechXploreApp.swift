f02e796fa97eb8eef27d273e668e91f53b7e843f
Elene <elenedgebuadze83@gmail.com>
2026-03-12 13:53:28 +0400

    first commit

---
+++ TechXplore/TechXploreApp.swift
@@ -0,0 +1,17 @@
+//
+//  TechXploreApp.swift
+//  TechXplore
+//
+//  Created by Eorime on 12.03.26.
+//
+
+import SwiftUI
+
+@main
+struct TechXploreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
